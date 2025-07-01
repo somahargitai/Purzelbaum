46c0ce3cb37a7f54c94f996d9f9b0921e51c9790
Soma Hargitai <hargitai.soma@gmail.com>
2025-07-01 02:04:17 +0200

    listen 0.0.0.0

--- api/src/server.js
+++ api/src/server.js
@@ -45,8 +45,9 @@ app.get("/api/sentences", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Server running on 0.0.0.0:3005")
+})
 
 export default app;
